1e1fb87411f24f0c61a1c8e2a2956cbbfc1988ab
Lucas Cecareli <Lucas.cecareli.ribeiro@gmail.com>
2025-05-19 21:45:49 -0300

    - adicionando o pacote modelo e adicionando as classes Usuarios e Perguntas

---
+++ src/main/java/projetointegrador/poliedro/modelo/Usuario.java
@@ -0,0 +1,16 @@
+package projetointegrador.poliedro.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+    private String login, senha;
+
+}
+    

484e72b22954aac1bcaf874fb04c04fc7d125812
Matheus Fantini <matheus.fantini01@gmail.com>
2025-05-27 21:22:21 -0300

    feat: cria a classe PerguntaDAO

---
+++ src/main/java/projetointegrador/poliedro/persistencia/PerguntaDAO.java
@@ -0,0 +1,66 @@
+
+package projetointegrador.poliedro.persistencia;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import projetointegrador.poliedro.modelo.Pergunta;
+import projetointegrador.poliedro.modelo.PerguntaResposta;
+import projetointegrador.poliedro.modelo.Resposta;
+
+public class PerguntaDAO {
+
+        public void cadastrar(Pergunta p) throws Exception {
+            var fabricaDeConexoes = new ConnectionFactory();
+            var conexao = fabricaDeConexoes.obterConexao();
+
+            // Inserir pergunta
+           
+            System.out.println("ID da matéria:" + p.getIdMateria());
+            var sql = "INSERT INTO tb_pergunta(enunciado, id_materia, id_serie, dificuldade, dica_pergunta) VALUES (?, ?, ?, ?, ?)";
+            System.out.println("SQL usada:" +  sql);
+            PreparedStatement psPergunta = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            psPergunta.setString(1, p.getEnunciado());
+            psPergunta.setInt(2, p.getIdMateria());
+            psPergunta.setInt(3, p.getIdSerie());
+            psPergunta.setInt(4, p.getNivelDificuldade());
+            psPergunta.setString(5, p.getDica());
+            psPergunta.executeUpdate();
+
+            ResultSet rs = psPergunta.getGeneratedKeys();
+            int idPergunta = 0;
+            if (rs.next()) {
+                idPergunta = rs.getInt(1);
+            }
+
+            psPergunta.close();
+
+            // Inserir respostas
+            var sqlResp = "INSERT INTO tb_resposta(texto_resposta, id_pergunta) VALUES (?,?)";
+            var sqlRespPerg = "INSERT INTO tb_pergunta_resposta(id_resposta, id_pergunta, correta) VALUES (?, ?, ?)";
+            for (PerguntaResposta r : p.getRespostas()) {
+                PreparedStatement psResp = conexao.prepareStatement(sqlResp, Statement.RETURN_GENERATED_KEYS);
+                psResp.setString(1, r.getResposta().getTexto());
+                psResp.setInt(2, idPergunta);
+                psResp.executeUpdate();
+
+                ResultSet rsResp = psResp.getGeneratedKeys();
+                int idResposta = 0;
+                if (rsResp.next()) {
+                    idResposta = rsResp.getInt(1);
+                }
+                psResp.close();
+
+                PreparedStatement psRespPerg = conexao.prepareStatement(sqlRespPerg);
+                psRespPerg.setInt(1, idResposta);
+                psRespPerg.setInt(2, idPergunta);
+                psRespPerg.setBoolean(3, r.isCorreta());
+                psRespPerg.execute();
+                psRespPerg.close();
+            }
+
+            conexao.close();
+        }
+    }
+
+

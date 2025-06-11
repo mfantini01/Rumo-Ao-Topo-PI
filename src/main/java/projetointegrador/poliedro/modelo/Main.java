5a3675c9b8041d2dcbc774c7c4b9dc38c9677dab
Matheus Fantini <matheus.fantini01@gmail.com>
2025-06-11 04:35:05 -0300

    classe Main

---
+++ src/main/java/projetointegrador/poliedro/modelo/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetointegrador.poliedro.modelo;
+
+import projetointegrador.poliedro.telas.TelaInicial;
+
+/**
+ *
+ * @author mathe
+ */
+public class Main {
+    public static void main(String [] args){
+        var ti = new TelaInicial();
+        ti.setVisible(true);
+    }
+}

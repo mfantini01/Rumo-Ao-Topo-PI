b4ca25d5c22dec2a4272b4e5cbdeef90c0ba4ecb
Matheus Fantini <matheus.fantini01@gmail.com>
2025-06-10 21:34:48 -0300

    adiciona um novo construtor na classe Pergunta

--- src/main/java/projetointegrador/poliedro/modelo/Pergunta.java
+++ src/main/java/projetointegrador/poliedro/modelo/Pergunta.java
@@ -1,43 +1,63 @@
 
 package projetointegrador.poliedro.modelo;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Pergunta {
-        private String enunciado;
-        private int idMateria;
-        private int idSerie;
-        private int nivelDificuldade;
-        private String dica;
-        private List<PerguntaResposta> respostas; 
-        
-        private String nomeMateria;
-        private String nomeSerie;
-        
-        public Pergunta(String texto){
-            this.enunciado = texto;
-            respostas = new ArrayList<>();
-        }
+
+    private int id;
+    private String enunciado;
+    private int idMateria;
+    private int idSerie;
+    private int nivelDificuldade;
+    private String dica;
+    private List<PerguntaResposta> respostas = new ArrayList<>();
+
+    private String nomeMateria;
+    private String nomeSerie;
+
+    public Pergunta(String texto) {
+        this.enunciado = texto;
+        this.respostas = new ArrayList<>();
+    }
 
     @Override
     public String toString() {
-        return "Pergunta{" + "texto=" + enunciado + ", respostas=" + respostas + '}';
+        return "Pergunta{" + "enunciado=" + enunciado + ", respostas=" + respostas + '}';
     }
-        
-        
-
-    void exibirPergunta() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public Pergunta(int id, String enunciado, String dica) {
+        this.id = id;
+        this.enunciado = enunciado;
+        this.dica = dica;
+    }
+   
+    public void exibirPergunta() {
+        System.out.println("Enunciado: " + enunciado);
+        System.out.println("Dica: " + dica);
+        System.out.println("Matéria: " + nomeMateria);
+        System.out.println("Série: " + nomeSerie);
+        System.out.println("Nível de Dificuldade: " + nivelDificuldade);
+        System.out.println("Respostas:");
+        for (int i = 0; i < respostas.size(); i++) {
+            System.out.println((i + 1) + ") " + respostas.get(i).getResposta().getTexto());
+        }
     }
 
     
+    public boolean verificarResposta(int indice) {
+        if (indice < 0 || indice >= respostas.size()) {
+            return false;
+        }
+        return respostas.get(indice).isCorreta();
+    }
 }
 
     
